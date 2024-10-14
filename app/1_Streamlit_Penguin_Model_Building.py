822bbda6f9888ead188dc3ba74a989f8ca095963
Aidil Adham <aidiladham@invokeisdata.com>
2024-10-14 12:55:53 +0800

    feat:insert pickle

---
+++ app/1_Streamlit_Penguin_Model_Building.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Penguin Prediction App
+         
+This app predicts the **Palmer Penguin** species!
+         
+Data obtained from the [palmerpenguins package](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+""")
+
+st.sidebar.header("User Input Features")
+
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+""")
+
+#Collect user input features into dataframe
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        island = st.sidebar.selectbox("Island",("Biscoe","Dream","Torgersen"))
+        sex = st.sidebar.selectbox("Sex",("male","female"))
+        bill_length_mm = st.sidebar.slider("Bill length (mm)", 32.1,59.6,43.9)
+        bill_depth_mm = st.sidebar.slider("Bill depth (mm)", 13.1,21.5,17.2)
+        flipper_length_mm = st.sidebar.slider("Flipper length (mm)", 172.0,231.0,201.0)
+        body_mass_g = st.sidebar.slider("Body mass (g)", 2700.0,6300.0,4207.0)
+        data = {"island": island,
+                "bill_length_mm": bill_length_mm,
+                "bill_depth_mm": bill_depth_mm,
+                "flipper_length_mm" : flipper_length_mm,
+                "body_mass_g" : body_mass_g,
+                "sex": sex}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+
+#Combines user input features with entire penguin dataset
+#This will be useful for the encoding phase
+penguins_raw = pd.read_csv("penguins_cleaned.csv")
+penguins = penguins_raw.drop(columns=["species"])
+df = pd.concat([input_df,penguins],axis=0)
+
+# Encoding of ordinal features
+
+encode = ["sex","island"]
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df,dummy], axis=1)
+    del df[col]
+df = df[:1]
